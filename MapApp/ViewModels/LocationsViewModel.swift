7f2f714adcde5ef15f56660cb62e18abeb77d99e
Saurabh Jaiswal <sjsaurabhjais@gmail.com>
2025-04-09 16:54:43 +0530

    Custom animated menu using a list

--- MapApp/ViewModels/LocationsViewModel.swift
+++ MapApp/ViewModels/LocationsViewModel.swift
@@ -21,8 +21,12 @@ class LocationsViewModel: ObservableObject {
         }
     }
     
+    // Current region on map
     @Published var mapRegion: MKCoordinateRegion = MKCoordinateRegion()
     let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    
+    // Show list of locations
+    @Published var showLocationsList: Bool = false
     
     init() {
         let locations = LocationsDataService.locations
@@ -37,6 +41,20 @@ class LocationsViewModel: ObservableObject {
                 center: location.coordinates,
                 span: mapSpan
             )
+        }
+    }
+    
+    func toggleLocationsList() {
+        withAnimation(.easeInOut) {
+            showLocationsList = !showLocationsList
+        }
+    }
+    
+    
+    func showNextLocation(location: Location) {
+        withAnimation(.easeInOut) {
+            mapLocation = location
+            showLocationsList = false
         }
     }
 }
